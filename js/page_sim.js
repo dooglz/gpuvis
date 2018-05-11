1e88aee54a169e5de0cb30b148f95fde41a2ee0e
Sam Serrels <doogly1@gmail.com>
2018-05-11 23:00:16 +0100

    SVG work

--- js/page_sim.js
+++ js/page_sim.js
@@ -2,7 +2,7 @@ function main_sim() {
     btn_go = $("#simgobtn");
     console.log("Hello sim");
     initvis();
-    if(!is(program)|| !is(isa)){
+    if (!is(program) || !is(isa)) {
         Warn("Load a Program+ISA First!")
         btn_go.prop("disabled", true);
         return;
@@ -13,88 +13,91 @@ function main_sim() {
 
 //let sim_ticks = [];
 const cus = 56
-const simdlanes=16;
-const simdunits=4;
+const simdlanes = 16;
+const simdunits = 4;
 const wfSize = simdlanes * simdunits;
 const sgprs = 12;
 const vgprs = 7;
 
-function getIsa(code){
-    let isacode = isa.codes.find(function(e) {
+function getIsa(code) {
+    let isacode = isa.codes.find(function (e) {
         return e.opcode === code;
     });
-    if(isacode === undefined){
-        console.error("unkown opcode",code);
+    if (isacode === undefined) {
+        console.error("unkown opcode", code);
     }
     return isacode;
 }
 
-function registerDecode(reg){
-    let split = function(reg){
+function registerDecode(reg) {
+    let split = function (reg) {
         let m = reg.indexOf(":");
         let l = reg.indexOf("[");
         let r = reg.indexOf("]");
-        let low = parseInt(reg.slice(l+1,m));
-        let high = parseInt(reg.slice(m+1,r));
-        return Array((high-low)+1).fill().map((e,i)=>i+low);
+        let low = parseInt(reg.slice(l + 1, m));
+        let high = parseInt(reg.slice(m + 1, r));
+        return Array((high - low) + 1).fill().map((e, i) => i + low);
     }
 
-    if(reg.startsWith("0x")){
-        return {type:"constant",val:parseInt(reg)};
+    if (reg.startsWith("0x")) {
+        return {type: "constant", val: parseInt(reg)};
     }
-    if(reg.startsWith("v")) {
-        if (reg.indexOf(":") != -1){
-            return {type:"vsplit",val:split(reg)};
+    if (reg.startsWith("v")) {
+        if (reg.indexOf(":") != -1) {
+            return {type: "vsplit", val: split(reg)};
         }
-        return {type:"v",val:parseInt(reg.slice(1))};
+        return {type: "v", val: parseInt(reg.slice(1))};
     }
-    if(reg.startsWith("s")) {
-        if (reg.indexOf(":") != -1){
-            return {type:"ssplit",val:split(reg)};
+    if (reg.startsWith("s")) {
+        if (reg.indexOf(":") != -1) {
+            return {type: "ssplit", val: split(reg)};
         }
-        return {type:"s",val:parseInt(reg.slice(1))};
+        return {type: "s", val: parseInt(reg.slice(1))};
     }
-    if(!isNaN(parseInt(reg))){
-        return {type:"constant",val:parseInt(reg)};
+    if (!isNaN(parseInt(reg))) {
+        return {type: "constant", val: parseInt(reg)};
     }
-    console.error("Can't decode register",reg);
-    return {type:"idklol",val:reg};
+    console.error("Can't decode register", reg);
+    return {type: "idklol", val: reg};
 }
 
 var globalTick = 0;
+var globalMaxTick = 0;
 var globalRegList = [];
 var globalSRegList = [];
 var globalVRegList = [];
-var globalUnknownWrites =0;
-var globalUnknownReads =0;
+var globalUnknownWrites = 0;
+var globalUnknownReads = 0;
 var globalVIdleTicks = 0;
 var globalVWorkTicks = 0;
-var usedCUs =0;
+var usedCUs = 0;
 var occupancy = [];
-function Launchsim(){
+
+function Launchsim() {
     try {
         //setup regs
         globalRegList = [];
         globalSRegList = [];
         globalVRegList = [];
-        globalUnknownWrites =0;
-        globalUnknownReads =0;
+        globalUnknownWrites = 0;
+        globalUnknownReads = 0;
         globalIdleTicks = 0;
         let opcode = "";
         let kernels = 1;
         let cus = [];
-        for(let i =0; i < Math.ceil(kernels / wfSize); ++i){
+        for (let i = 0; i < Math.ceil(kernels / wfSize); ++i) {
             cus.push(new ComputeUnit(i));
         }
         usedCUs = cus.length;
         console.log("Launching sim with CU:", cus.length);
         let not_finished = true;
         let tickcount = 0;
-        while(not_finished && tickcount < 100) {
+        while (not_finished && tickcount < 100) {
             $("#simstatus").text(tickcount + " / " + (program.kernel.asm.instructions.length - 1));
             not_finished = false;
             tickcount++;
             globalTick = tickcount;
+            globalMaxTick = globalTick;
             for (let c of cus) {
                 not_finished |= c.tick();
             }
@@ -102,50 +105,80 @@ function Launchsim(){
         console.log("done");
         calcStats(cus.length);
         updateGPUVis();
-    }catch (e) {
+        postSim();
+    } catch (e) {
         console.error(e);
-        $("#simstatus").html("sim Error <br>"+JSON.stringify(e));
+        $("#simstatus").html("sim Error <br>" + JSON.stringify(e));
     }
 }
 
-function calcStats(cuCount){
-    let reglists = {Total:globalRegList,Scaler:globalSRegList,Vector:globalVRegList};
-    let totals = {Scaler:cuCount*sgprs,Vector:cuCount*simdunits*simdlanes*vgprs}
-    totals.Total=totals.Scaler + totals.Vector;
+function calcStats(cuCount) {
+    let reglists = {Total: globalRegList, Scaler: globalSRegList, Vector: globalVRegList};
+    let totals = {Scaler: cuCount * sgprs, Vector: cuCount * simdunits * simdlanes * vgprs}
+    totals.Total = totals.Scaler + totals.Vector;
     let ret = "";
     for (let rl in reglists) {
         let readregs = 0;
         let writtenregs = 0;
         let totalwrites = 0;
-        let rotalreads =0;
+        let rotalreads = 0;
         for (let r of reglists[rl]) {
-            if(r.reads.length > 0){readregs++;}
-            if(r.writes.length > 0){writtenregs++;}
-            totalwrites +=r.writes.length;
+            if (r.reads.length > 0) {
+                readregs++;
+            }
+            if (r.writes.length > 0) {
+                writtenregs++;
+            }
+            totalwrites += r.writes.length;
             rotalreads += r.reads.length;
         }
-        ret+="<p>"+rl+" registers written to: "+ writtenregs +" / " + totals[rl] +
-            "<br>"+rl+" registers read from: "+ readregs +" / " + totals[rl] +
-            "<br>"+rl+" register reads: "+ rotalreads +
-            "<br>"+rl+" register writes: "+ totalwrites + "</p>";
+        ret += "<p>" + rl + " registers written to: " + writtenregs + " / " + totals[rl] +
+            "<br>" + rl + " registers read from: " + readregs + " / " + totals[rl] +
+            "<br>" + rl + " register reads: " + rotalreads +
+            "<br>" + rl + " register writes: " + totalwrites + "</p>";
     }
-    ret+="<p>Unaccounted Writes: " +globalUnknownWrites+
-        "<br>Unaccounted Reads: " +globalUnknownReads+"</p>";
+    ret += "<p>Unaccounted Writes: " + globalUnknownWrites +
+        "<br>Unaccounted Reads: " + globalUnknownReads + "</p>";
 
-    ret+="<p>Acive SIMD Ticks:" +globalVWorkTicks+
-        "<br>Idle SIMD Ticks: " +globalVIdleTicks+
-        "<br>Local Occupancy: " + sigFigs((globalVWorkTicks/(globalVWorkTicks+globalVIdleTicks))*100,4)+'%'+
-        "<br>Global Occupancy: " + sigFigs((globalVWorkTicks/((globalVWorkTicks+globalVIdleTicks)*cus))*100,4)+ "%</p>";
+    ret += "<p>Acive SIMD Ticks:" + globalVWorkTicks +
+        "<br>Idle SIMD Ticks: " + globalVIdleTicks +
+        "<br>Local Occupancy: " + sigFigs((globalVWorkTicks / (globalVWorkTicks + globalVIdleTicks)) * 100, 4) + '%' +
+        "<br>Global Occupancy: " + sigFigs((globalVWorkTicks / ((globalVWorkTicks + globalVIdleTicks) * cus)) * 100, 4) + "%</p>";
 
-    occupancy[0] = (globalVWorkTicks/(globalVWorkTicks+globalVIdleTicks));
+    occupancy[0] = (globalVWorkTicks / (globalVWorkTicks + globalVIdleTicks));
     $("#simresults").html(ret);
 }
 
+function postSim() {
+    $("#simseekstart").click(function () {
+        return seek(0);
+    });
+    $("#simseekback").click(function () {
+        return seek(globalTick - 1);
+    });
+    $("#simseeknext").click(function () {
+        return seek(globalTick + 1);
+    });
+    $("#simseekend").click(function () {
+        return seek(globalMaxTick);
+    });
+    seek(globalTick)
+}
+
+function seek(tick) {
+    if (isNaN(tick) || tick < 0 || tick > globalMaxTick) {
+        console.error(tick);
+        return;
+    }
+    globalTick = tick;
+    $("#simseek").html("Cycle:" + tick);
+}
+
 class ComputeUnit {
-    constructor(gid,lid) {
+    constructor(gid, lid) {
         this.gid = gid;
         this.lid = lid;
-        this.pc =0;
+        this.pc = 0;
         this.sALU = 0;
         this.LDS = 0;
         this.vALUs = [];
@@ -156,57 +189,60 @@ class ComputeUnit {
         this.tickreads = [];
         this.tickwrites = [];
         this.lastTick = 0;
-        this.execMask = Array(simdunits*simdlanes).fill(false);
+        this.execMask = Array(simdunits * simdlanes).fill(false);
         this.execMask[0] = true;
-        for(let i =0; i < sgprs; ++i){
-            let r =new Register(this.gid+'_sgpr_'+i);
+        for (let i = 0; i < sgprs; ++i) {
+            let r = new Register(this.gid + '_sgpr_' + i);
             globalSRegList.push(r)
             this.SGPR.push(r);
         }
-        for(let i =0; i < simdunits; ++i){
-            this.vALUs.push(new V_ALU(this.gid+'_'+i,i));
+        for (let i = 0; i < simdunits; ++i) {
+            this.vALUs.push(new V_ALU(this.gid + '_' + i, i));
         }
     }
+
     get opcode() {
         return program.kernel.asm.instructions[this.pc].opcode;
     }
+
     get operand() {
         return program.kernel.asm.instructions[this.pc].operand;
     }
 
-    writeTo(loc,what = 1){
-        let o = {tick:globalTick,loc:loc,what:what};
+    writeTo(loc, what = 1) {
+        let o = {tick: globalTick, loc: loc, what: what};
         this.writes.push(o);
         this.tickwrites.push(o);
         let rd = registerDecode(loc);
-        if(rd.type === "s"){
+        if (rd.type === "s") {
             this.SGPR[rd.val].write(what);
-        }else if(rd.type === "ssplit"){
-            for(let r of rd.val){
+        } else if (rd.type === "ssplit") {
+            for (let r of rd.val) {
                 this.SGPR[r].write(what);
             }
-        }else{
+        } else {
             //TODO
         }
     }
-    readFrom(loc){
-        let o = {tick:globalTick,loc:loc};
+
+    readFrom(loc) {
+        let o = {tick: globalTick, loc: loc};
         this.reads.push(o);
         this.tickreads.push(o);
         let rd = registerDecode(loc);
-        if(rd.type === "s"){
+        if (rd.type === "s") {
             this.SGPR[rd.val].read();
-        }else if(rd.type === "ssplit"){
-            for(let r of rd.val){
+        } else if (rd.type === "ssplit") {
+            for (let r of rd.val) {
                 this.SGPR[r].read();
             }
-        }else{
+        } else {
             //TODO
         }
     }
 
     tick() {
-        if(this.done || this.opcode == "s_endpgm" ){
+        if (this.done || this.opcode == "s_endpgm") {
             this.done = true;
             console.log(this, "cu done");
             return false;
@@ -215,29 +251,29 @@ class ComputeUnit {
         this.tickreads = [];
         this.tickwrites = [];
         let isac = getIsa(this.opcode);
-        console.log(this.id, "cu tick",this,this.opcode,isac );
-        if(isac.type === "v" || isac.type === "flat"){
-            for(let s of this.vALUs){
-                s.tick(isac,this.opcode,this.operand,this.execMask);
+        console.log(this.id, "cu tick", this, this.opcode, isac);
+        if (isac.type === "v" || isac.type === "flat") {
+            for (let s of this.vALUs) {
+                s.tick(isac, this.opcode, this.operand, this.execMask);
                 this.tickreads.push(s.tickreads);
                 this.tickwrites.push(s.tickwrites);
             }
-            console.log(this.id, "v reads",this.tickreads ,"v writes",this.tickwrites);
-        }else if(isac.type === "s"){
+            console.log(this.id, "v reads", this.tickreads, "v writes", this.tickwrites);
+        } else if (isac.type === "s") {
             //do reads
-            if(isac.r){
+            if (isac.r) {
                 for (let r of isac.r) {
                     this.readFrom(this.operand[r]);
                 }
             }
             //do writes
-            if(isac.w){
+            if (isac.w) {
                 for (let w of isac.w) {
-                 this.writeTo(this.operand[w]);
+                    this.writeTo(this.operand[w]);
                 }
             }
-            console.log(this.id, "s reads",this.tickreads ,"s writes",this.tickwrites);
-        }else{
+            console.log(this.id, "s reads", this.tickreads, "s writes", this.tickwrites);
+        } else {
             console.error(isac.type);
         }
 
@@ -247,7 +283,7 @@ class ComputeUnit {
 }
 
 class V_ALU {
-    constructor(gid,lid) {
+    constructor(gid, lid) {
         this.gid = gid;
         this.lid = lid;
         this.lanes = [];
@@ -255,26 +291,27 @@ class V_ALU {
         this.writes = [];
         this.tickreads = [];
         this.tickwrites = [];
-        for(let i =0; i < simdlanes; ++i){
-            this.lanes.push(new V_ALU_lane(this.gid+'_'+i,(this.lid*simdlanes)+i));
+        for (let i = 0; i < simdlanes; ++i) {
+            this.lanes.push(new V_ALU_lane(this.gid + '_' + i, (this.lid * simdlanes) + i));
         }
     }
-    tick(isac,opcode,operand,execMask){
+
+    tick(isac, opcode, operand, execMask) {
         this.tickreads = [];
         this.tickwrites = [];
-        for(let l of this.lanes) {
-            l.tick(isac,opcode,operand,execMask);
+        for (let l of this.lanes) {
+            l.tick(isac, opcode, operand, execMask);
             this.tickreads.push(l.tickreads);
             this.tickwrites.push(l.tickwrites);
         }
         this.reads.push(this.tickreads);
         this.writes.push(this.tickwrites);
-       // console.info(this.id, "reads",tickreads,"writes",tickwrites);
+        // console.info(this.id, "reads",tickreads,"writes",tickwrites);
     }
 }
 
 class V_ALU_lane {
-    constructor(gid,lid){
+    constructor(gid, lid) {
         this.gid = gid;
         this.lid = lid;
         this.VGPR = [];
@@ -283,81 +320,86 @@ class V_ALU_lane {
         this.tickreads = [];
         this.tickwrites = [];
         this.lasttick = 0;
-        for(let i =0; i < vgprs; ++i){
-            let r = new Register(this.gid+'_vgpr_'+i)
+        for (let i = 0; i < vgprs; ++i) {
+            let r = new Register(this.gid + '_vgpr_' + i)
             globalVRegList.push(r)
             this.VGPR.push(r);
         }
     }
-    writeIndirect(loc,what = 1){
-        let o = {tick:globalTick,loc:"IND",what:what};
+
+    writeIndirect(loc, what = 1) {
+        let o = {tick: globalTick, loc: "IND", what: what};
         this.writes.push(o);
         this.tickwrites.push(o);
         globalUnknownWrites++;
     }
-    readIndirect(loc){
-        let o = {tick:globalTick,loc:"IND"};
+
+    readIndirect(loc) {
+        let o = {tick: globalTick, loc: "IND"};
         this.reads.push(o);
         this.tickreads.push(o);
         globalUnknownReads++;
     }
-    writeTo(loc,what = 1){
-        let o = {tick:globalTick,loc:loc,what:what};
+
+    writeTo(loc, what = 1) {
+        let o = {tick: globalTick, loc: loc, what: what};
         this.writes.push(o);
         this.tickwrites.push(o);
         let rd = registerDecode(loc);
-        if(rd.type === "v"){
+        if (rd.type === "v") {
             this.VGPR[rd.val].write(what);
-        }else if(rd.type === "vsplit"){
-           for(let r of rd.val){
-               this.VGPR[r].write(what);
-           }
-        }else{
+        } else if (rd.type === "vsplit") {
+            for (let r of rd.val) {
+                this.VGPR[r].write(what);
+            }
+        } else {
             //TODO
         }
     }
-    readFrom(loc){
-        let o = {tick:globalTick,loc:loc};
+
+    readFrom(loc) {
+        let o = {tick: globalTick, loc: loc};
         this.reads.push(o);
         this.tickreads.push(o);
         let rd = registerDecode(loc);
-        if(rd.type === "v"){
+        if (rd.type === "v") {
             this.VGPR[rd.val].read();
-        }else if(rd.type === "vsplit"){
-            for(let r of rd.val){
+        } else if (rd.type === "vsplit") {
+            for (let r of rd.val) {
                 this.VGPR[r].read();
             }
-        }else{
+        } else {
             //TODO
         }
     }
-    tick(isac,opcode,operand,execMask){
+
+    tick(isac, opcode, operand, execMask) {
         this.tickreads = [];
         this.tickwrites = [];
         this.lasttick = globalTick;
-        if(!execMask[this.lid]){
+        if (!execMask[this.lid]) {
             globalVIdleTicks++;
             return;
         }
         globalVWorkTicks++;
         //do reads
-        if(isac.r){
+        if (isac.r) {
             for (let r of isac.r) {
                 this.readFrom(operand[r]);
             }
         }
-        if(isac.ri){
+        if (isac.ri) {
             for (let r of isac.ri) {
                 this.readIndirect(operand[r]);
             }
         }
         //do writes
-        if(isac.w){
+        if (isac.w) {
             for (let w of isac.w) {
                 this.writeTo(operand[w]);
             }
         }
-        if(isac.wi){
+        if (isac.wi) {
             for (let w of isac.wi) {
                 this.writeIndirect(operand[w]);
             }
@@ -365,19 +407,21 @@ class V_ALU_lane {
     }
 }
 
-class Register{
-    constructor(id = "unnamed"){
+class Register {
+    constructor(id = "unnamed") {
         this.value = 0;
         this.reads = [];
         this.writes = [];
         this.id = id;
         globalRegList.push(this);
     }
-    read(){
+
+    read() {
         this.reads.push(globalTick);
 
     }
-    write(data = 1){
+
+    write(data = 1) {
         this.writes.push(globalTick);
     }
 }
--- js/page_sim_vis.js
+++ js/page_sim_vis.js
@@ -19,17 +19,25 @@ function initvis() {
 }
 
 var d_cus = [];
+var cuscon;
+var cucon;
+var toprow;
+var bottomrow;
 
 function buildGPU() {
     d_cus = [];
-    svgcon.append("rect")
-        .attr("x", schw - 50)
-        .attr("y", 0)
-        .attr("width", 100)
-        .attr("height", 100);
+
 
     let bestfit = {dif: 1000, g: cus}
-    const aspect = scw / (sch - 120);
+    toprow = 0.2 * sch;
+    bottomrow = sch - toprow;
+    svgcon.append("rect")
+        .attr("x", schw - (0.5 * toprow))
+        .attr("y", 0)
+        .attr("width", toprow)
+        .attr("height", toprow);
+
+    const aspect = scw / (sch - toprow);
     for (let g = cus; g > 0; g--) {
         const colls = g;
         const rows = Math.ceil(cus / colls);
@@ -43,17 +51,22 @@ function buildGPU() {
     console.log(bestfit);
     const colls = bestfit.g;
     const rows = Math.ceil(cus / colls);
-    let cudim = Math.min(scw / (colls), (sch - 120) / (rows));
+    let cudim = Math.min(scw / (colls), bottomrow / rows);
     let gap = cudim;
     cudim -= 10;
+    cuscon = svgcon.append("g").attr("transform", "translate(0," + toprow + ")");
 
     for (let i = 0; i < cus; i++) {
-        let g = svgcon.append("g")
+        let g = cuscon.append("g")
             .attr("transform",
                 "translate(" +
                 (gap * (i % colls)) + "," +
-                (110 + gap * (Math.floor(i / colls))) + ")")
-            .classed("computeUnit", true);
+                (gap * (Math.floor(i / colls))) + ")")
+            .classed("computeUnit", true)
+            .attr("id", i)
+            .on("click", function (d) {
+                return zoom(this);
+            });
         g.append("rect")
             .attr("width", cudim)
             .attr("height", cudim);
@@ -62,14 +75,123 @@ function buildGPU() {
             .attr("x", cudim / 2)
             .attr("y", cudim / 2)
             .attr("dy", ".35em") //todo find out what this is
-            .append('tspan').text("CU:"+i).attr('dy', '.9em');
+            .append('tspan').text("CU:" + i).attr('dy', '.9em');
         d_cus.push(g);
     }
+
+    cucon = ghelper(svgcon, "actualComputeUnit", "Compute Unit", 0, toprow, scw, bottomrow).attr("visibility", "hidden ").on("click", nop);
+    gap = bottomrow / 5.2;
+    let things = [["lds", "LDS"], ["l1c", "L1 Cache"], ["bmu", "Branch & Msg Unit"], ["scheduler", "Scheduler"], ["tu", "Texture Units"]];
+    for (let i = 0; i < things.length; i++) {
+        ghelper(cucon, things[i][0], things[i][1], 20, 20 + (i * gap), gap * 0.85, gap * 0.85, true);
+    }
+    ghelper(cucon, "salu", "Scaler ALU", gap * 1.5, 20, gap * 0.85, gap * 0.85, true);
+    ghelper(cucon, "sgpr", "Scaler GPR", gap * 1.5, 20 + gap, gap * 0.85, gap * 0.85, true);
+    {
+        let vgap = (bottomrow - 40) / simdunits;
+        let unitwidth = scw - (gap * 3.5);
+        for (let i = 0; i < simdunits; i++) {
+            let v = ghelper(cucon, "valu", "Simd Unit " + i, (gap * 3), 20 + (i * vgap), unitwidth, vgap * 0.85, false);
+            let lanewidth = (unitwidth - 40) / simdlanes;
+            for (let j = 0; j < simdlanes; j++) {
+                let lane = ghelper(v, "lane", "Lane " + j, 20 + (lanewidth * j), 20, lanewidth * 0.85, vgap * 0.6, false);
+
+                let vgprheight = (vgap * 0.5) / vgprs;
+                for (let k = 0; k < vgprs; k++) {
+                    ghelper(lane, "vgpr", k, 2, (vgap * 0.1) + (k * vgprheight), lanewidth * 0.80, vgprheight * 0.9, true);
+
+                }
+
+            }
+        }
+    }
+    svgcon.on("click", reset);
+
 }
 
-function updateGPUVis(){
-    svgcon.selectAll(".computeUnit").classed("used", function(d, i) { return i < usedCUs; });
-    svgcon.selectAll(".used > text").append('tspan')
-        .attr('dy', '.9em').text(function(d, i) {return (occupancy[i]*100) });
+function ghelper(parent, css, text, x, y, w, h, labelIn) {
+    let g = parent.append("g").attr("transform", "translate(" + x + "," + y + ")").classed(css, true);
+    g.rect = g.append("rect").attr("width", w).attr("height", h);
+    g.label = g.append("text").text(text);
+    if (labelIn) {
+        g.label.attr("x", w * 0.1)
+        g.label.attr("y", h / 2)
+        g.label.call(wrap, w * 0.8);
+    }
 
+    return g;
+}
+
+var dd;
+
+function zoom(d) {
+    dd = d;
+    console.log(d);
+    var t = cuscon.transition()
+        .duration(d3.event.altKey ? 7500 : 750)
+        .attr("transform", "translate(" + (scw - (0.25 * scw)) + ",0) scale(0.25,0.25)");
+
+    cucon.label.text("Compute Unit:" + dd.attributes.id.value);
+    cucon.transition().duration(d3.event.altKey ? 7500 : 750).attr("visibility", "visible");
+    d3.event.stopPropagation();
+
+}
+
+function nop() {
+    console.log("nop");
+    d3.event.stopPropagation();
+}
+
+function reset() {
+    console.log("reset");
+    var t = cuscon.transition()
+        .duration(d3.event.altKey ? 7500 : 750)
+        .attr("transform", "translate(0," + toprow + ") scale(1.0,1.0)");
+    cucon.transition().duration(d3.event.altKey ? 7500 : 750).attr("visibility", "hidden");
+    d3.event.stopPropagation();
+}
+
+
+function updateGPUVis() {
+    cuscon.selectAll(".computeUnit").classed("used", function (d, i) {
+        return i < usedCUs;
+    });
+    cuscon.selectAll(".used > text").append('tspan')
+        .attr('dy', '.9em').text(function (d, i) {
+        return (occupancy[i] * 100)
+    });
+//cucon.attr("transform","translate("+(scw-(0.25*scw))+",0) scale(0.25,0.25)");
+}
+
+function wrap(text, width) {
+    text.each(function () {
+        var text = d3.select(this),
+            words = text.text().split(/\s+/).reverse(),
+            word,
+            line = [],
+            lineNumber = 0,
+            lineHeight = 1.1, // ems
+            x = text.attr("x"),
+            y = text.attr("y"),
+            dy = 0, //parseFloat(text.attr("dy")),
+            tspan = text.text(null)
+                .append("tspan")
+                .attr("x", x)
+                .attr("y", y)
+                .attr("dy", dy + "em");
+        while (word = words.pop()) {
+            line.push(word);
+            tspan.text(line.join(" "));
+            if (tspan.node().getComputedTextLength() > width) {
+                line.pop();
+                tspan.text(line.join(" "));
+                line = [word];
+                tspan = text.append("tspan")
+                    .attr("x", x)
+                    .attr("y", y)
+                    .attr("dy", ++lineNumber * lineHeight + dy + "em")
+                    .text(word);
+            }
+        }
+    });
 }
